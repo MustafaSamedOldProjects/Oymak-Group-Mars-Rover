0e3a0b932908757856a9c7451c3224831ca42972
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 00:03:22 +0300

    Helper'ların metot imzalar yaratıldı.
    ICordinateHelper
    IRobotHelper
    eklendi.

---
+++ Cozum/Interfaces/Helpers/ICordinateHelper.cs
@@ -0,0 +1,16 @@
+﻿using Cozum.Concrete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Interfaces.Helpers
+{
+    public interface ICordinateHelper
+    {
+        Tuple<int, int> GetValuesFromUserForCordinateSystem();
+        List<Cordinate> CreateCordinateSystem(int xLength, int yLength);
+        void ShowCordinateSystemInConsole(List<Cordinate> rectangleCordinate);
+    }
+}
---
+++ Cozum/Interfaces/Helpers/IRobotHelper.cs
@@ -0,0 +1,18 @@
+﻿using Cozum.Concrete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Interfaces.Helpers
+{
+    public interface IRobotHelper
+    {
+        Tuple<int, int, string> GetRobotPosition();
+        Robot CreateRobot(int xPosition, int yPosition, string Direction);
+        string RobotPlatoWalk();
+        void RobotFinalPosition(Robot robot, string commandToRobot);
+
+    }
+}
