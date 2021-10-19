078968579c4b72d43205525457cb0508ebd21eac
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-19 23:59:22 +0300

    Model Concrete eklendi.

---
+++ Cozum/Concrete/Models/Cordinate.cs
@@ -0,0 +1,16 @@
+﻿using Cozum.Interfaces.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Concrete.Models
+{
+    public class Cordinate : ICordinate
+    {
+
+        public int cordinateX { get; set; }
+        public int cordinateY { get; set; }
+    }
+}
---
+++ Cozum/Concrete/Models/Robot.cs
@@ -0,0 +1,17 @@
+﻿using Cozum.Interfaces.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Concrete.Models
+{
+    public class Robot : IRobot
+    {
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
+        public string Direction { get; set; }
+
+    }
+}
