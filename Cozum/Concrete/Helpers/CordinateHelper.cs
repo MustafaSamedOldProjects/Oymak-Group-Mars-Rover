7cd8c9504396f38b318e663484623759a94ba402
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 00:09:23 +0300

    Helper'ların Concrete'leri yaatıldı. Ve Interface'lerden türetildi.

---
+++ Cozum/Concrete/Helpers/CordinateHelper.cs
@@ -0,0 +1,28 @@
+﻿using Cozum.Concrete.Models;
+using Cozum.Interfaces.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Concrete.Helpers
+{
+    public class CordinateHelper : ICordinateHelper
+    {
+        public List<Cordinate> CreateCordinateSystem(int xLength, int yLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int> GetValuesFromUserForCordinateSystem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowCordinateSystemInConsole(List<Cordinate> rectangleCordinate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cozum/Concrete/Helpers/RobotHelper.cs
@@ -0,0 +1,33 @@
+﻿using Cozum.Concrete.Models;
+using Cozum.Interfaces.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Concrete.Helpers
+{
+    public class RobotHelper : IRobotHelper
+    {
+        public Robot CreateRobot(int xPosition, int yPosition, string Direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int, string> GetRobotPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RobotFinalPosition(Robot robot, string commandToRobot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RobotPlatoWalk()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
