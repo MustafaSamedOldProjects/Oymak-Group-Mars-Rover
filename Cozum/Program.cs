5a3a64f94c53cf1dc7104a93a10bb15614a47bdd
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 00:38:16 +0300

    Program.cs yazıldı.

--- Cozum/Program.cs
+++ Cozum/Program.cs
@@ -3,6 +3,7 @@ using Cozum.Concrete.Models;
 using Cozum.Interfaces.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 
 namespace Cozum
 {
@@ -11,14 +12,33 @@ namespace Cozum
         static void Main(string[] args)
         {
             ServiceProvider serviceProvider = new ServiceCollection()
-                                           .AddScoped<IRobot, Robot>()
-                                           .AddScoped<ICordinate, Cordinate>()
                                            .AddScoped<ICordinateHelper, CordinateHelper>()
                                            .AddScoped<IRobotHelper, RobotHelper>()
                                            .BuildServiceProvider();
 
+            ICordinateHelper cordinateHelper = serviceProvider.GetService<ICordinateHelper>();
+            Tuple<int, int> cordinates = cordinateHelper.GetValuesFromUserForCordinateSystem();
+            List<Cordinate> rectangleCordinate = cordinateHelper.CreateCordinateSystem(cordinates.Item1, cordinates.Item2);
+            
+            IRobotHelper robotHelper = serviceProvider.GetService<IRobotHelper>();
+            string exitStatus = "";
 
-            Console.Read();
+            List<Robot> robots = new List<Robot>();
+            while (exitStatus != "-1")
+            {
+                Tuple<int, int, string> robotPosition = robotHelper.GetRobotPosition();
+                var createdRobot = robotHelper.CreateRobot(robotPosition.Item1, robotPosition.Item2, robotPosition.Item3);
+                robots.Add(createdRobot);
+                string commands = robotHelper.RobotPlatoWalk();
+                robotHelper.RobotFinalPosition(createdRobot, commands);
+                Console.WriteLine("Sonuçları görmek için -1 yazınız, farklı robot girişi yapmak için enter'a basmanız yeterli.");
+                exitStatus = Console.ReadLine();
+                if (exitStatus == "-1")
+                {
+                    robotHelper.WriteRobotsToConsole(robots);
+                }
+            }
+
         }
     }
 }
