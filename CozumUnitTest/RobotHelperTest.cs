af6993c72de1004a1d352eee9aa467a1602332a1
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 18:26:24 +0300

    Yeni bir test RobotHelper metodu için yazıldı.

--- CozumUnitTest/RobotHelperTest.cs
+++ CozumUnitTest/RobotHelperTest.cs
@@ -34,18 +34,33 @@ namespace CozumUnitTest
 
         [Theory]
         [InlineData(1, 2, "N", "LMLMLMLMM")]
-        [InlineData(10, 10, "S","LMRMLMRM")]
+        [InlineData(10, 10, "S", "LMRMLMRM")]
         [InlineData(3, 3, "E", "MMRMMRMRRM")]
         public void Test_For_RobotFinalPosition(int xPosition, int yPosition, string direction, string command)
         {
-            Robot robot =  _robotHelper.RobotFinalPosition(new Robot() 
-            { 
-                xPosition= xPosition,
+            Robot robot = _robotHelper.RobotFinalPosition(new Robot()
+            {
+                xPosition = xPosition,
                 yPosition = yPosition,
                 Direction = direction
             }, command);
             Assert.Equal(robot.Direction, direction);
 
+        }
+
+
+        [Theory]
+        [InlineData("13N")]
+        [InlineData("2   4   S")]
+        [InlineData(" 3 2 W")]
+        [InlineData("45  W")]
+        public void Test_For_GetRobotPosition(string position)
+        {
+            position = position.ToUpper().Replace(" ", "");
+            Tuple<int, int, string> robotPosition = _robotHelper.GetRobotPosition(position);
+            Assert.Equal(robotPosition.Item1, Convert.ToInt32(position.Substring(0, 1)));
+            Assert.Equal(robotPosition.Item2, Convert.ToInt32(position.Substring(1, 1)));
+            Assert.Equal(robotPosition.Item3, position.Substring(2, 1));
         }
     }
 }
