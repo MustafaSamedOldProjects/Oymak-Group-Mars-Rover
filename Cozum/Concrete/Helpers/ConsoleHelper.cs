1aab08761e4d84ddd7198190d64f54adc666d057
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 18:17:51 +0300

    Console'dan okuma işlemi artık ConsoleHelper'dan yapılacak.

---
+++ Cozum/Concrete/Helpers/ConsoleHelper.cs
@@ -0,0 +1,39 @@
+﻿using Cozum.Interfaces.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Concrete.Helpers
+{
+    public class ConsoleHelper : IConsoleHelper
+    {
+        public string ReadFromConsole()
+        {
+            string input = Console.ReadLine();
+
+            string inputRemovedSpaces = RemoveSpaces(input);
+
+            return inputRemovedSpaces.ToUpper();
+
+        }
+        public string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", ""); ;
+        }
+        public string ReadFromConsoleForPlato()
+        {
+            string input = Console.ReadLine();
+
+            return input.ToUpper();
+
+        }
+
+
+        public void WriteMessageToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Cozum/Interfaces/Helpers/IConsoleHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cozum.Interfaces.Helpers
+{
+    public interface IConsoleHelper
+    {
+        string ReadFromConsole();
+        string RemoveSpaces(string input);
+        public string ReadFromConsoleForPlato();
+        void WriteMessageToConsole(string message);
+    }
+}
