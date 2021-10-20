3e7132d6421d0cf3ce4c3772adb5afaff0db579c
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 18:23:57 +0300

    Program.cs de ConsoleHelper kullanıldı.
    ConsoleHelper'dan gelen Input'lar gerekli metotlarada parametre olarak verildi.

--- Cozum/Program.cs
+++ Cozum/Program.cs
@@ -14,22 +14,33 @@ namespace Cozum
             ServiceProvider serviceProvider = new ServiceCollection()
                                            .AddScoped<ICordinateHelper, CordinateHelper>()
                                            .AddScoped<IRobotHelper, RobotHelper>()
+                                           .AddScoped<IConsoleHelper, ConsoleHelper>()
                                            .BuildServiceProvider();
 
+            IConsoleHelper consoleHelper = serviceProvider.GetService<IConsoleHelper>();
+           
             ICordinateHelper cordinateHelper = serviceProvider.GetService<ICordinateHelper>();
-            Tuple<int, int> cordinates = cordinateHelper.GetValuesFromUserForCordinateSystem();
+            consoleHelper.WriteMessageToConsole("Lütfen Bir Dikdörtgen Giriniz");
+            string inputCordinate = consoleHelper.ReadFromConsoleForPlato();
+            Tuple<int, int> cordinates = cordinateHelper.GetValuesFromUserForCordinateSystem(inputCordinate);
             List<Cordinate> rectangleCordinate = cordinateHelper.CreateCordinateSystem(cordinates.Item1, cordinates.Item2);
             cordinateHelper.ShowCordinateSystemInConsole(rectangleCordinate);
+            
+
             IRobotHelper robotHelper = serviceProvider.GetService<IRobotHelper>();
             string exitStatus = "";
 
             List<Robot> robots = new List<Robot>();
             while (exitStatus != "-1")
             {
-                Tuple<int, int, string> robotPosition = robotHelper.GetRobotPosition();
+                consoleHelper.WriteMessageToConsole("Robot'un x,y ve yönünü(E,W,N,S) girin.");
+                string inputRobotPosition = consoleHelper.ReadFromConsole();
+                Tuple<int, int, string> robotPosition = robotHelper.GetRobotPosition(inputRobotPosition);
                 var createdRobot = robotHelper.CreateRobot(robotPosition.Item1, robotPosition.Item2, robotPosition.Item3);
                 robots.Add(createdRobot);
-                string commands = robotHelper.RobotPlatoWalk();
+                Console.WriteLine("Robot'un gideceği yönü R,L,M şeklinde belirtiniz.");
+                string inputForPlatoWalk =  consoleHelper.ReadFromConsole();
+                string commands = robotHelper.RobotPlatoWalk(inputForPlatoWalk);
                 robotHelper.RobotFinalPosition(createdRobot, commands);
                 Console.WriteLine("Sonuçları görmek için -1 yazınız, farklı robot girişi yapmak için enter'a basmanız yeterli.");
                 exitStatus = Console.ReadLine();
