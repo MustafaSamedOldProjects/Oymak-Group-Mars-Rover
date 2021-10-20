abb51f67b5776da1aea8d896548d5af15caa9339
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-20 22:34:52 +0300

    Autufac ayarlamaları yapıldı.

--- Cozum/Program.cs
+++ Cozum/Program.cs
@@ -1,4 +1,6 @@
-﻿using Cozum.Concrete.Helpers;
+﻿using Autofac;
+using Cozum.Applications;
+using Cozum.Concrete.Helpers;
 using Cozum.Concrete.Models;
 using Cozum.Interfaces.Helpers;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,48 +11,20 @@ namespace Cozum
 {
     public class Program
     {
+        private static IContainer CompositionRoot()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Application>();
+            builder.RegisterType<ConsoleHelper>().As<IConsoleHelper>() ;
+            builder.RegisterType<RobotHelper>().As<IRobotHelper>() ;
+            builder.RegisterType<CordinateHelper>().As<ICordinateHelper>() ;
+            return builder.Build();
+        }
         public static void Main(string[] args)
         {
-            ServiceProvider serviceProvider = new ServiceCollection()
-                                           .AddScoped<ICordinateHelper, CordinateHelper>()
-                                           .AddScoped<IRobotHelper, RobotHelper>()
-                                           .AddScoped<IConsoleHelper, ConsoleHelper>()
-                                           .BuildServiceProvider();
 
-            IConsoleHelper consoleHelper = serviceProvider.GetService<IConsoleHelper>();
-           
-            ICordinateHelper cordinateHelper = serviceProvider.GetService<ICordinateHelper>();
-            consoleHelper.WriteMessageToConsole("Lütfen Bir Dikdörtgen Giriniz");
-            string inputCordinate = consoleHelper.ReadFromConsoleForPlato();
-            Tuple<int, int> cordinates = cordinateHelper.GetValuesFromUserForCordinateSystem(inputCordinate);
-            consoleHelper.WriteMessageToConsole("Plato aşağıdaki gibidir.");
-            List<Cordinate> rectangleCordinate = cordinateHelper.CreateCordinateSystem(cordinates.Item1, cordinates.Item2);
-            cordinateHelper.ShowCordinateSystemInConsole(rectangleCordinate);
-            
-
-            IRobotHelper robotHelper = serviceProvider.GetService<IRobotHelper>();
-            string exitStatus = "";
-
-            List<Robot> robots = new List<Robot>();
-            while (exitStatus != "-1")
-            {
-                consoleHelper.WriteMessageToConsole("Robot'un x,y ve yönünü(E,W,N,S) girin. Örneğin : 1 2 N");
-                string inputRobotPosition = consoleHelper.ReadFromConsole();
-                Tuple<int, int, string> robotPosition = robotHelper.GetRobotPosition(inputRobotPosition);
-                var createdRobot = robotHelper.CreateRobot(robotPosition.Item1, robotPosition.Item2, robotPosition.Item3);
-                robots.Add(createdRobot);
-                Console.WriteLine("Robot'un gideceği yönü R,L,M şeklinde belirtiniz.");
-                string inputForPlatoWalk =  consoleHelper.ReadFromConsole();
-                string commands = robotHelper.RobotPlatoWalk(inputForPlatoWalk);
-                robotHelper.RobotFinalPosition(createdRobot, commands);
-                Console.WriteLine("Sonuçları görmek için -1 yazınız, farklı robot girişi yapmak için enter'a basmanız yeterli.");
-                exitStatus = Console.ReadLine();
-                if (exitStatus == "-1")
-                {
-                    robotHelper.WriteRobotsToConsole(robots);
-                }
-            }
-
+            var runable = CompositionRoot().Resolve<Application>();
+            runable.Run();
         }
     }
 }
